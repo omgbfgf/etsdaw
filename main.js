6ee22b47b390adb486aec4d0b564137dc8c385dd
Advik <91060569+omgbfgf@users.noreply.github.com>
2023-02-02 16:56:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+status="";
+video='';
+function preload(){
+video=createVideo("video.mp4");
+
+}
+
+function setup(){
+canvas=createCanvas(480,380);
+canvas.center();
+video.hide();
+}
+
+function start(){
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status:detecting objects";
+}
+
+function modelLoaded(){
+    console.log("once again you saw it");
+    status=true;
+    video.loop();
+    video.speed(1.5);
+    video.volume(0);
+    
+}
+
+function draw(){
+    image(video,0,0,480,380)
+}
